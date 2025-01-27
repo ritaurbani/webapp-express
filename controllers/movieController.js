257924f4aac99d5b9041ee07630a583dcc33bf24
ritaurbani <ritaurbani@gmail.com>
2025-01-27 14:49:06 +0100

    feat: aggiunta filtri backend

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -2,25 +2,52 @@ const db_Connection = require("../data/db_Connection")
 
 //INDEX
 const index = (req, res, next) => {
-    //QUERY SENZA FILTRO
-
-    //controllo se ci sono query-string-params e le prelevo
-    //stringa che uso come filtro nel ?search=la  search e' la key dell oggetto, per avere valore: filters.search
+   //controllo se ci sono query-string-params e le prelevo
+    //stringa che uso come filtro nel ?search=la search e'la key dell oggetto req.query che contiene i parametri della query string passati nella URL
     const filters = req.query;
-    //questi 2 valore sono le cose che differiscono tra ricerca con o senza filtri
-    let sql = "SELECT * FROM movies"
-    const params = []
+    //ogni chiave di questo oggetto e`un parametro nella URL
+    // const { title, genre, year } = req.query;
 
-    //QUERY CON FILTRI
+    //stringa di base - seleziona tutti i films
+    let sql = "SELECT * FROM movies"
+    //parametri dinamici da passare alla query - per evitare injection - valore della key
+    const params = []
+    //tutti i filtri che saranno aggiunti alla query
+    const conditions = []
+
+    //Aggiungiamo filtri alla query - se search e presente aggiungiamo condizione per filtrare i risultati in base al titolo
     if (filters.search) {
-        sql += `
-        WHERE title LIKE ?`;
-        params.push(`%${filters.search}%`);
-    } 
+        conditions.push("title LIKE ?") 
+        params.push(`%${filters.search}%`)
+    }
+
+    // if (filters.genre) {
+    //     conditions.push("genre = ?");
+    //     params.push(`%${filters.search}%`)
+    // }
+
+    //CICLO PER AGGIUNGERE ALTRI FILTRI - ITERA SUI PARAMETRI DI RICERCA
+    for (const key in req.query) {
+        if (key !== "search") {
+            //SE PARAMETRO NON E SEARCH AGGIUNGE CONDIZIONE ALLA QUERY (genre = ?)
+            conditions.push(`${key} = ?`);
+            //AGGIUNGE VALORE DEL PARAMETRO (genre=comedy, aggiunge comedy)
+            params.push(req.query[key])
+        }
+    }
+
+    //costruire query finale - SE CI SONO FILTRI ALLORA..AGGIUNGI WHERE...
+    if (conditions.length > 0) {
+        //UNISCE TUTTE LE CONDIZIONI CON L OPERATORE AND EX: ..WHERE title LIKE ? AND genre =? AND year = ?
+        sql += ` WHERE ${conditions.join(" AND ")}`
+    }
+
+    //ESECUZIONE QUERY - SE C E ERRORE INVIA UN ERRORE AL MIDDLEWARE DI GESTIONE DEGLI ERRORI
         db_Connection.query(sql, params, (err, movies) => {
             if (err) {
-                return next(new Error("Errore interno del server"))
+                return next(new Error("Errore interno del server")) //Error(err.message)
             }
+
             return res.status(200).json({
                 status: "success",
                 data: movies,
